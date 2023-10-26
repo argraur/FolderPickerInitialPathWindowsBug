204d42f5a2ee1ea744c348adb15e7fab99abca15
argraur <me@argraur.dev>
2023-10-26 23:27:19 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FolderPickerInitialPath
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Storage;
+
+namespace FolderPickerInitialPath
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnInitialPathPickerClicked(object sender, EventArgs e)
+        {
+            var result = await FolderPicker.PickAsync(InitialPath.Text, CancellationToken.None);
+            if (result != null && result.Folder != null)
+            {
+                await Toast.Make(result.Folder.Path).Show();
+            }
+        }
+
+        private async void OnNoInitialPathPickerClicked(object sender, EventArgs e)
+        {
+            var result = await FolderPicker.PickAsync(CancellationToken.None);
+            if (result != null && result.Folder != null)
+            {
+                await Toast.Make(result.Folder.Path).Show();
+            }
+        }
+    }
+
+}
